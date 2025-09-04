f008751fe52b937a160494a419ceebb52f6007a2
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-09-04 11:08:06 -0300

    feat: change font and layout

--- src/app/api/page.jsx
+++ src/app/api/page.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 import Link from 'next/link';
+import styles from './api.module.css';
 
 export default function Api() {
   const [bruxos, setBruxos] = useState([]);
@@ -25,22 +26,10 @@ export default function Api() {
   };
 
   return (
-    <div className="bg-[url('https://images6.alphacoders.com/130/thumb-1920-1302546.jpg')] bg-fixed bg-cover bg-center min-h-screen p-10">
-      <button className='mt-6 gap-6 flex flex-row'>
-        <Link
-          href="/sobre"
-          className='bg-blue-500 text-white font-bold px-6 py-3 rounded-lg shadow  hover:bg-blue-900'>
-          SAIBA MAIS
-        </Link>
-        <Link
-          href="/perfil"
-          className='bg-blue-500 text-white font-bold px-6 py-3 rounded-lg shadow  hover:bg-blue-900'>
-          PERFIL
-        </Link>
-      </button>
+    <div className="bg-[url('https://images6.alphacoders.com/130/thumb-1920-1302546.jpg')] bg-fixed bg-cover bg-center min-h-screen p-10 flex flex-col justify-center">
       <div className="max-w-4x1 mx-auto">
-        <h1 className="text-5xl font-bold text-center mb-8">
-          Bruxos de Harry Potter
+        <h1 className={styles.hpTitle}>
+          <span className="font-wizard">Bruxos de Harry Potter</span>
         </h1>
 
         <div className="text-center mb-8">
@@ -61,14 +50,14 @@ export default function Api() {
           
           <div
             key={bruxos.id}
-            className="bg-white/50 rounded-1g shadow-md rounded-lg flex flex-row justify-center items-center p-6 space-x-20 transition-transform duration-300 hover:scale-105 active:scale-95"
+            className="bg-white/50 rounded-1g shadow-md rounded-lg flex flex-col-reverse justify-center items-center transition-transform duration-300 hover:scale-105 active:scale-95 p-6"
           >
-            <div>
-              <h3 className="font-bold text-1g text-gray-800 text-2xl w-35">
+            <div className="w-full p-4 flex flex-col justify-center items-center">
+              <h3 className="font-bold text-1g text-gray-800 text-2xl w-ful items-center justify-center flex">
                 {bruxos.name}
               </h3>
-              <p className="text-gray-900">{bruxos.gender}</p>
-              <div className="text-gray-900">
+              {/* <p className="text-gray-900">{bruxos.gender}</p> */}
+              {/* <div className="text-gray-900">
                 {bruxos.alternate_names.length > 0 ? (
                   bruxos.alternate_names.map((name, index) => (
                     <p key={index}>{name}</p>
@@ -76,7 +65,7 @@ export default function Api() {
                 ) : (
                   <p>Sem nomes alternativos</p>
                 )}
-              </div>
+              </div> */}
               <p
                 className={
                   bruxos.house === "Gryffindor" ? "text-red-600 font-semibold flex"
@@ -100,9 +89,9 @@ export default function Api() {
                   <img src="https://img.icons8.com/?size=100&id=6Xj9rtUmyktD&format=png&color=000000" alt="Ravenclaw" className="w-6 h-6 ml-2" />
                 )}
               </p>
-              <p className="text-gray-900">{bruxos.ancestry}</p>
-              <p className="text-gray-900">{bruxos.patronus}</p>
-              <p className="text-gray-900">{bruxos.dateOfBirth}</p>
+              {/* <p className="text-gray-900">{bruxos.ancestry}</p> */}
+              {/* <p className="text-gray-900">{bruxos.patronus}</p> */}
+              {/* <p className="text-gray-900">{bruxos.dateOfBirth}</p> */}
             </div>
 
             <img
@@ -111,7 +100,7 @@ export default function Api() {
                 "https://camo.githubusercontent.com/e7260310f5d1a8a9473a908e039f348a459078b0ba1876d12fbe0a26c8a0e1a7/68747470733a2f2f7a7562652e696f2f66696c65732f706f722d756d612d626f612d63617573612f33363664616462316461323032353338616531333332396261333464393030362d696d6167652e706e67"
               }
               alt={bruxos.name || "Nome não carregado"}
-              className="w-50 h-60 object-cover rounded-2xl"
+              className="w-50 h-50 object-cover rounded-full"
             />
           </div>
         ))}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,22 @@
 import "./globals.css";
+import localFont from 'next/font/local';
+
+const WizardWorldSimplified = localFont({
+  src: [
+    {
+      path: '../../public/fonts/WizardWorldSimplified.ttf',
+      weight: '400',
+      style: 'normal'
+    },
+  ],
+  variable: '--font-wizard',
+  display: 'swap',
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR">
-      <body>
+    <html lang="pt-BR" className={WizardWorldSimplified.variable}>
+      <body className={WizardWorldSimplified.className}>
         {children}
       </body>
     </html>
